5a61d1321b94b21a63dd21687b7473e1adf41884
Carlos Ortega <the_king_00carlos@hotmail.com>
2021-06-07 22:58:26 -0600

    Configure tailwind css config file and added tailwind forms plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 module.exports = {
-  purge: [],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'ghost-white': '#F8F9FC',
+        Xiketic: '#06051A',
+        'oxford-blue': '#0A092B',
+        'neon-blue': '#4A69FE',
+        'persian-blue': '#3036C9',
+        'cyber-grape': '#4C4268',
+        'cadet-blue-crayola': '#A0A7BC',
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 };
