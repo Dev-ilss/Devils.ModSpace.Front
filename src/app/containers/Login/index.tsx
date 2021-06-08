77f146747436ddec140493afbec72ee2350eb240
Carlos Ortega <the_king_00carlos@hotmail.com>
2021-06-07 23:28:21 -0600

    Added login and signup content (forms)

--- src/app/containers/Login/index.tsx
+++ src/app/containers/Login/index.tsx
@@ -4,9 +4,49 @@
  *
  */
 import React, { memo } from 'react';
+import { Link } from 'react-router-dom';
 
 interface Props {}
 
 export const Login = memo((props: Props) => {
-  return <div></div>;
+  return (
+    <>
+      <div className="container mx-auto">
+        <h2 className="text-center text-oxford-blue font-bold text-3xl mb-12">
+          Inicia Sesión
+        </h2>
+        <form className="w-4/5 mx-auto mb-4" onSubmit={e => console.log(e)}>
+          <input
+            className="form-input rounded border-neon-blue border-2 py-4 w-full mb-8"
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Email"
+          />
+          <input
+            className="form-input rounded border-neon-blue border-2 py-4 w-full mb-8"
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Contraseña"
+          />
+          <input
+            type="submit"
+            value="Iniciar sesión"
+            className="w-full bg-neon-blue hover:bg-persian-blue py-4  rounded text-white font-semibold text-lg border-b-8 border-rounded cursor-pointer border-persian-blue"
+          />
+        </form>
+        <p className="text-center font-semibold">
+          ¿No tienes cuenta? Registrate gratis{' '}
+          <Link
+            to="/sign-up"
+            className="text-neon-blue hover:text-persian-blue"
+          >
+            aquí
+          </Link>
+          .
+        </p>
+      </div>
+    </>
+  );
 });
--- src/app/containers/SignUp/index.tsx
+++ src/app/containers/SignUp/index.tsx
@@ -4,9 +4,78 @@
  *
  */
 import React, { memo } from 'react';
+import { Link } from 'react-router-dom';
 
 interface Props {}
 
 export const SignUp = memo((props: Props) => {
-  return <div></div>;
+  return (
+    <>
+      <div className="container mx-auto">
+        <h2 className="text-center text-oxford-blue font-bold text-3xl mb-12">
+          Registrate
+        </h2>
+        <form className="w-4/5 mx-auto mb-4" onSubmit={e => console.log(e)}>
+          <input
+            className="form-input rounded border-neon-blue border-2 py-4 w-full mb-8"
+            type="text"
+            name="user"
+            placeholder="Usuario"
+          />
+          <div className="w-full grid grid-cols-2 gap-2">
+            <input
+              className="form-input rounded border-neon-blue border-2 py-4 col-span-1 col-start-1 mb-8"
+              type="text"
+              name="name"
+              placeholder="Nombre"
+            />
+            <input
+              className="form-input rounded border-neon-blue border-2 py-4 col-span-1 col-start-2 mb-8"
+              type="text"
+              name="lastName"
+              placeholder="Apellido"
+            />
+          </div>
+          <input
+            className="form-input rounded border-neon-blue border-2 py-4 w-full mb-8"
+            type="email"
+            name="email"
+            placeholder="Email"
+          />
+          <input
+            className="form-input rounded border-neon-blue border-2 py-4 w-full mb-8"
+            type="tel"
+            name="tel"
+            placeholder="Teléfono"
+          />
+          <div className="w-full grid grid-cols-2 gap-2">
+            <input
+              className="form-input rounded border-neon-blue border-2 py-4 col-span-1 col-start-1 mb-8"
+              type="password"
+              name="password"
+              placeholder="Contraseña"
+            />
+            <input
+              className="form-input rounded border-neon-blue border-2 py-4 col-span-1 col-start-2 mb-8"
+              type="password"
+              name="confirmPassword"
+              placeholder="Confirma contraseña"
+            />
+          </div>
+          <input
+            type="submit"
+            value="Crear cuenta"
+            className="w-full bg-neon-blue hover:bg-persian-blue py-4  rounded text-white font-semibold text-lg border-b-8 border-rounded cursor-pointer border-persian-blue"
+          />
+        </form>
+        <p className="text-center font-semibold">
+          ¿Ya tienes cuenta? Inicia Sesión{' '}
+          <Link to="/login" className="text-neon-blue hover:text-persian-blue">
+            aquí
+          </Link>
+          .
+        </p>
+      </div>
+    </>
+  );
 });
