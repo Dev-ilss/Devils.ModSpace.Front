ec117dfb1a16091d9a5e4af38cc7d035b53183e9
Carlos Ortega <the_king_00carlos@hotmail.com>
2021-08-12 16:33:19 -0600

    Proximamente stuff

--- src/app/components/SideBar/index.tsx
+++ src/app/components/SideBar/index.tsx
@@ -5,9 +5,12 @@
  */
 import React, { memo } from 'react';
 import { IUser } from 'types';
+import { Link } from 'react-router-dom';
 
 import { useAuthSlice } from '../../slices/AuthSlice';
 import { useDispatch } from 'react-redux';
+
+import { DASHBOARD_LINK } from 'utils/constants';
 
 import profilePic from '../../../assets/profile.png';
 
@@ -37,13 +40,19 @@ export const SideBar = memo(({ user }: Props) => {
         <nav className="h-100 flex-grow">
           <ul className="h-full flex flex-col">
             <li className="p-3">
-              <p>Inicio</p>
+              <Link to={DASHBOARD_LINK}>Inicio</Link>
             </li>
             <li className="p-3">
-              <p>Perfil</p>
+              <p>
+                Perfil{' '}
+                <span className="text-gray-300 text-sm">¡Proximamente!</span>
+              </p>
             </li>
             <li className="p-3">
-              <p>Mis Mods</p>
+              <p>
+                Mis Mods{' '}
+                <span className="text-gray-300 text-sm">¡Proximamente!</span>
+              </p>
             </li>
             <li className="mt-auto mb-24 p-3">
               <button onClick={() => dispatch(actions.logout())}>Salir</button>
